9197b21aa9fdeb14fa01bc9cd5f61d4c20f3ea1e
Maxence Tourneux <tourneuxmaxence@gmail.com>
2021-11-12 16:09:32 +0100

    canvas.js clean code and add comments

--- src/js/canvas.js
+++ src/js/canvas.js
@@ -1,5 +1,7 @@
+//Declaration of variables
 var canvas = document.getElementById('dice')
 var ctx = canvas.getContext('2d')
+//Functions initializeDice
 let initializeDice = () => {
   ctx.fillStyle = '#ffffff'
   ctx.fillRect(0 , 0, canvas.width, canvas.height)
@@ -10,105 +12,83 @@ let initializeDice = () => {
   ctx.font = `${sizeText}px Arial`
   ctx.fillText('?', canvas.width / 2, canvas.width / 2)
 }
-
+//Clear canvas and create a new empty 
 let clearCanvas = () => {
   ctx.clearRect(0,0, canvas.width, canvas.height)
   ctx.fillStyle = '#ffffff'
   ctx.fillRect(0 , 0, canvas.width, canvas.height)
 }
-
+//Number 1
 let numberOne = () => {
   clearCanvas()
   centerPoint()
 }
-
+//Number 2
 let numberTwo = () => {
   clearCanvas()
   diagonalUp()
 }
-
+//Number 3
 let numberThree = () => {
   clearCanvas()
   centerPoint()
   diagonalDown()
 }
-
+//Number 4
 let numberFour = () => {
   clearCanvas()
   diagonalUp()
   diagonalDown()
 }
-
+//Number 5
 let numberFive = () => {
   clearCanvas()
   centerPoint()
   diagonalUp()
   diagonalDown()
 }
-
+//Number 6
 let numberSixt = () => {
   clearCanvas()
   diagonalUp()
   diagonalDown()
   line()
 }
-
+//-+---+-
+//-+-o-+-
+//-+---+-
 let centerPoint = () => {
-  //-+---+-
-  //-+-o-+-
-  //-+---+-
   ctx.beginPath()
   ctx.arc(canvas.width / 2, canvas.width / 2, 10, 0, 2 * Math.PI)
   ctx.fillStyle = '#000000'
   ctx.fill()
 }
-
+//-+---o-
+//-+-+-+-
+//-o---+-
 let diagonalUp = () => {
-  //-+---+-
-  //-+-+-+-
-  //-o---+-
   ctx.beginPath()
   ctx.arc(canvas.width / 4.5, canvas.width - (canvas.width / 4.5), 10, 0, 2 * Math.PI)
-  ctx.fillStyle = '#000000'
-  ctx.fill()
-  //-+---o-
-  //-+-+-+-
-  //-+---+-
-  ctx.beginPath()
   ctx.arc(canvas.width - (canvas.width / 4.5), (canvas.width / 4.5), 10, 0, 2 * Math.PI)
   ctx.fillStyle = '#000000'
   ctx.fill()
 }
-
+//-o---+-
+//-+-+-+-
+//-+---o-
 let diagonalDown = () => {
-  //-o---+-
-  //-+-+-+-
-  //-+---+-
   ctx.beginPath()
   ctx.arc(canvas.width / 4.5, canvas.width / 4.5, 10, 0, 2 * Math.PI)
-  ctx.fillStyle = '#000000'
-  ctx.fill()
-  //-+---+-
-  //-+-+-+-
-  //-+---o-
-  ctx.beginPath()
   ctx.arc(canvas.width - (canvas.width / 4.5), canvas.width - (canvas.width / 4.5), 10, 0, 2 * Math.PI)
   ctx.fillStyle = '#000000'
   ctx.fill()
 }
-
+//-+---+-
+//-o-+-o-
+//-+---+-
 let line = () => {
-  //-+---+-
-  //-o-+-+-
-  //-+---+-
   ctx.beginPath()
   ctx.arc(canvas.width / 4.5, canvas.width / 2, 10, 0, 2 * Math.PI)
-  ctx.fillStyle = '#000000'
-  ctx.fill()
-  //-+---+-
-  //-+-+-o-
-  //-+---+-
-  ctx.beginPath()
   ctx.arc(canvas.width - (canvas.width / 4.5), canvas.width / 2, 10, 0, 2 * Math.PI)
   ctx.fillStyle = '#000000'
   ctx.fill()
